8a5add843abd4399017136bf6650e4e241283243
Kristian Söderblom <28917898+ksoderbl@users.noreply.github.com>
2018-06-27 16:51:26 +0300

    17: Multitexturing, added files

---
+++ src/TerrainTexture.h
@@ -0,0 +1,14 @@
+#ifndef TERRAINTEXTURE_H
+#define TERRAINTEXTURE_H
+
+#include "Headers.h"
+
+class TerrainTexture {
+public:
+	TerrainTexture(GLuint textureID);
+	GLuint getTextureId();
+private:
+	GLuint textureID;
+};
+
+#endif
